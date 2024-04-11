105c4b32538f9d6ddc2aca41de7dd662673a82df
BenteKavsek <35726@ma-web.nl>
2024-04-11 09:14:59 +0200

    les 5 af

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,28 +7,25 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-blue-100 font-mono text-center">
+        <div class="min-h-screen bg-blue-300 dark:bg-green-300 font-mono text-center">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-green-300 dark:bg-blue-300 shadow">
+                    <div class="max-w-7xl mx-auto p-20 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
 
-            <!-- Page Content -->
+
             <main>
                 {{ $slot }}
             </main>
